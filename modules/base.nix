ba77d68216abd3ba795cdc4b3b47bf64cb1e72ee
Austin Nason <austin.nason@schrodinger.com>
2025-09-30 11:24:39 -0700

    copy/paste error

--- modules/base.nix
+++ modules/base.nix
@@ -33,7 +33,7 @@
       viAlias = true;
       vimAlias = true;
     };
-    programs.nh = {
+    nh = {
       enable = true;
       clean.enable = true;
       clean.extraArgs = "--keep-since 4d --keep 3";
